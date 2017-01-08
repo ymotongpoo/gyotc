d5624fb1f65d0ad908c1e4842b24324deb4fc84c
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-01-08 12:05:16 +0900

    fix bug that first xhr.call sends requeest not to gyotc page but to original page

--- background.js
+++ background.js
@@ -12,17 +12,45 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
+/**
+ * gyotcURL returns web gyotaku link of url page.
+ * @param {string} url URL of target page.
+ * @return {string} Corresponding gyotaku URL page to url.
+ */
+function gyotcURL(url) {
+    return "https://gyo.tc/" + url;
+}
+
 chrome.webNavigation.onCommitted.addListener((details) => {
     if (["link", "typed", "start_page", "reload"].indexOf(details.transitionType) >= 0) {
         console.log(details.tabId, details.url);
         const xhr = new XHR(details.tabId);
         const protocol = new URL(details.url).protocol;
         if (["http:", "https:", "ftp:"].indexOf(protocol) >= 0) {
-            xhr.call(details.url);
+            xhr.call(gyotcURL(details.url));
         }
     }
 });
 
+const activeIcons = {
+    "16": "img/active16.png",
+    "24": "img/active24.png",
+    "32": "img/active32.png",
+    "64": "img/active64.png"
+}
+
+const inactiveIcons = {
+    "16": "img/icon16.png",
+    "24": "img/icon24.png",
+    "32": "img/icon32.png",
+    "64": "img/icon64.png"
+}
+
+/**
+ * XHR handles XMLHttpRequest tasks.
+ * @param {number} tabId active tag id to call XHR.
+ * @constructor
+ */
 class XHR {
     constructor(tabId) {
         this.tabId = tabId;
@@ -43,25 +71,34 @@ class XHR {
 
     /**
      * _callback is callback function for call.
-     * @param ev XHR callback event.
+     * @param {ProgressEvent} ev XHR callback event.
      */
     callback_(ev) {
         const resp = ev.target;
         const doc = resp.responseXML;
-        const alertDom = doc.getElementsByClassName("alert alert-dismissible alert-info");
-        if (alertDom.length === 0) {
+        const links = doc.getElementsByClassName("col-xs-12 col-sm-12 col-md-12")[1];
+        console.log(links);
+        if (links === undefined) {
+            chrome.browserAction.setIcon({
+                    "tabId": this.tabId,
+                    "path": inactiveIcons
+            }, () => {
+                chrome.tabs.get(this.tabId, (tab) => {
+                    // TODO(ymotongpoo): add appropriate tasks.
+                });
+            });
+            return;
+        }
+
+        const a = links.getElementsByTagName("a");
+        if (a.length > 0) {
             chrome.browserAction.setIcon({
                 "tabId": this.tabId,
-                "path": {
-                    "16": "img/active16.png",
-                    "24": "img/active24.png",
-                    "32": "img/active32.png",
-                    "64": "img/active64.png"
-                }
+                "path": activeIcons
             }, () => {
                 chrome.tabs.get(this.tabId, (tab) => {
                     chrome.browserAction.onClicked.addListener((tab) => {
-                        chrome.tabs.update(tab.id, {url: "http://gyo.tc/" + tab.url});            
+                        chrome.tabs.update(tab.id, {url: gyotcURL(tab.url)});            
                     });
                 });
                 this.hasGyotc = true;
